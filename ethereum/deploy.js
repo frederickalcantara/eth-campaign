2aeb2f5b70e863c48743b43a5c2a0a5f4b330ae7
frederickalcantara <frederickaalcantara@gmail.com>
2019-01-11 23:47:31 -0500

    Added deploy file and refactored deployment for contract code

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,7 +1,7 @@
 const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
-const { interface, bytecode } = require('./compile');
-const { mnemonic, APIRoute } = require('./env/truflle-wallet');
+const compiledFactory = require('./build/CampaignFactory.json');
+const { mnemonic, APIRoute } = require('../env/truffle-wallet');
 
 const provider = new HDWalletProvider(mnemonic, APIRoute);
 const web3 = new Web3(provider);
@@ -10,11 +10,12 @@ const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
   console.log(`Attempting to deploy from account: ${accounts[0]}`);
-  const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: `0x${bytecode}` })
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: `0x${compiledFactory.bytecode}` })
     .send({ from: accounts[0], gas: 1000000 });
 
-  console.log(interface);
   console.log(`Contracted deployed to ${result.options.address}`);
 };
 
